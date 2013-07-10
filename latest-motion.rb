89c1adc4771dfda0ba01c1203ba025a97bdc23f4
pauly <pauly@clarkeology.com>
2013-07-10 13:52:10 +0000

    put a border around the changing bit of the pic

--- latest-motion.rb
+++ latest-motion.rb
@@ -7,15 +7,14 @@ today = Time.now.strftime '%Y-%m-%d'
 latest = { }
 minsize = 9000
 images = [ ]
-Dir.entries( folder ).each do | f |
+files = Dir.entries( folder ).sort_by { |a| File.stat( folder + a ).mtime }
+files.each do | f |
   if f !~ /^\./
-    stat = File.stat( folder + '/' + f )
-    if today == stat.mtime.strftime( '%Y-%m-%d' )
+    if 'jpg' === f[ f.length - 3, f.length - 1 ]
+      stat = File.stat( folder + '/' + f )
       if stat.size > minsize
-      	# puts folder + '/' + f
-      	# puts stat.inspect
-	latest = f
-	images << f
+        latest = f
+        images << f
       end
     end
   end
@@ -30,6 +29,8 @@ html = <<-end
     <title>#{title}</title>
     <style type="text/css">
       body { font-family: arial, verdana, sans-serif; }
+      #image-container { position: relative; }
+      #highlight { position: absolute; }
     </style>
   </head>
   <body>
@@ -38,9 +39,10 @@ html = <<-end
 	<div class="col">
 	  <h1>#{title}</h1>
 	  <p class="intro">#{intro}</p>
-	  <div>
+	  <div id="image-container">
 	    <img id="image" src="#{latest}" title="#{latest}" />
 	    <p id="caption">#{latest}</p>
+	    <div id="highlight"></div>
 	  </div>
 	</div>
       </div>
@@ -57,31 +59,51 @@ html = <<-end
       };
       var image = document.getElementById( 'image' );
       var caption = document.getElementById( 'caption' );
+      var highlight = document.getElementById( 'highlight' );
       var images = #{images};
       var max = images.length - 1;
       var index = max;
       var load = function ( i ) {
-        console.log( 'loading image', i );
         if ( i < 0 ) i = max;
         if ( i > max ) i = 0;
-        console.log( 'image[', i, '] is ', images[i], 'max is', max );
 	if ( images[i] && ( 'jpg' === images[i].substr( -3, 3 ))) {
           caption.innerHTML = images[i];
           image.title = images[i];
           image.src = images[i];
+	  if ( /(\\d+)x(\\d+)-(\\d+)x(\\d+)/.exec( images[i] )) {
+	    if ( RegExp.$1 || RegExp.$2 || RegExp.$3 || RegExp.$4 ) {
+	      var width = RegExp.$1 / 1;
+	      var height = RegExp.$2 / 1;
+	      var x = RegExp.$3 / 1;
+	      var y = RegExp.$4 / 1;
+	      var left = Math.round( x - ( width / 2 ));
+	      var top = Math.round( y - ( height / 2 ));
+	      console.log( 'draw a box width', width, 'height', height, 'centre', x, y );
+	      highlight.style.visibility = 'visible';
+	      highlight.style.border = '1px #ff0 dashed';
+	      highlight.style.left = left + 'px';
+	      highlight.style.top = top + 'px';
+	      highlight.style.width = width + 'px';
+	      highlight.style.height = height + 'px';
+	    }
+	    else {
+	      highlight.style.visibility = 'hidden';
+	    }
+	  }
 	}
 	else {
           caption.innerHTML = 'No preview for ' + images[i];
 	}
+	return i;
       };
       attachEvent( document, 'keydown', function ( e ) {
         switch ( e.which || e.keyDown ) {
 	  case 37:
 	  case 38:
-            load( -- index ); break;
+            index = load( -- index ); break;
 	  case 39:
 	  case 40:
-            load( ++ index ); break;
+            index = load( ++ index ); break;
 	}
       } );
     </script>
