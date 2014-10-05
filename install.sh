720f25d62ebd111b4f4f45ab158599b5378b8d71
pauly <pauly@clarkeology.com>
2014-10-05 21:31:45 +0100

    tweaked motion settings

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 export EDITOR=vi
+sudo passwd pi
 sudo dpkg-reconfigure tzdata
 sudo apt-get update
 sudo apt-get upgrade
-# sudo aptitude install linux-headers-2.6-$(uname -r|sed 's,[^-]*-[^-]*-,,') ndiswrapper-utils-1.9 wireless-tools
-sudo apt-get install git-core
+sudo apt-get install -y git-core apache2 php5 libapache2-mod-php5 libjpeg62 libjpeg62-dev libavformat53 libavformat-dev libavcodec53 libavcodec-dev libavutil51 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev
 # git config --global user.email "you@example.com"
 # git config --global user.name "Your Name"
-sudo passwd pi
-sudo apt-get install apache2 php5 libapache2-mod-php5
-sudo service apache2 restart
-cd /var
-sudo git clone https://github.com/pauly/pi.git
-sudo mv pi/ www
-sudo apt-get install nginx
-cd www
+git clone https://github.com/pauly/pi-motion.git
+sudo cp motion.conf /etc/motion.conf
+sudo cp motion /usr/bin
+sudo cat /etc/rc.local | sed 's/exit/motion\n\nexit/' > /etc/rc.local
+# echo '\ndisable_camera_led=1' >> /boot/config.txt
+mkdir /var/www/motion
+cd /var/www
 wget http://www.raspberrypi.org/favicon.ico
