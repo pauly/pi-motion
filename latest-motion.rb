ffe545a83bf38c151949b28e08db861dea755fe5
pauly <pauly@clarkeology.com>
2014-10-07 21:41:03 +0100

    mobify + simplify

--- latest-motion.rb
+++ latest-motion.rb
@@ -4,7 +4,7 @@ require 'json'
 
 folder = ARGV.length ? ARGV[0] : Dir.pwd
 today = Time.now.strftime '%Y-%m-%d'
-minsize = 50000
+minsize = 18000
 images = [ ]
 files = Dir.entries( folder ).sort_by { |a| File.stat( folder + a ).mtime }
 files.each do | f |
@@ -26,21 +26,26 @@ html = <<-end
 <html>
   <head>
     <title>#{title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <style type="text/css">
       body { font-family: arial, verdana, sans-serif; }
       h1 { font-size: 1em; }
       h2 { font-size: 0.8em; font-weight: normal; }
       #image-container { position: relative; }
       #highlight { position: absolute; }
-      #prev, #next { position: absolute; top: 0px; width: 512px; height: 576px; float: left; }
+      #prev, #next { position: absolute; left: 0px; top: 0px; width: 512px; height: 576px; float: left; text-indent: 999em; }
       #next { margin-left: 512px; text-align: right; }
+      @media (max-width: 800px) {
+        h1, h2, p { font-size: 2em; }
+        img, #prev, #next { width: 100%; }
+      }
     </style>
   </head>
   <body>
     <div class="container">
       <div class="row">
         <div class="col">
-          <h1>#{title}</h1>
+          <h1><a href="">#{title}</a></h1>
           <h2>Way slow, loaded from a <a href="http://www.clarkeology.com/wiki/raspberry+pi">raspberry pi</a> in my house. Hmm, this is why my broadband appears so patchy...</h2>
           <div id="image-container">
             <img id="image" src="#{images.slice -1}" title="#{images.slice -1}"  width="1024" height="576" />
@@ -50,11 +55,13 @@ html = <<-end
           </div>
           <h2>Click left or left cursor key for the previous image, right for the next. Hmm how do I swipe?</h2>
           <div class="previousImages">
-            <img id="image" src="#{images.slice -2}" title="#{images.slice -2}" width="128" height="96" />
-            <img id="image" src="#{images.slice -3}" title="#{images.slice -3}" width="128" height="96" />
-            <img id="image" src="#{images.slice -4}" title="#{images.slice -4}" width="128" height="96" />
-            <img id="image" src="#{images.slice -5}" title="#{images.slice -5}" width="128" height="96" />
-            <img id="image" src="#{images.slice -6}" title="#{images.slice -6}" width="128" height="96" />
+            <a href="##{images.slice -2}"><img src="#{images.slice -2}" width="128" height="96" /></a>
+            <a href="##{images.slice -3}"><img src="#{images.slice -3}" width="128" height="96" /></a>
+            <a href="##{images.slice -4}"><img src="#{images.slice -4}" width="128" height="96" /></a>
+            <a href="##{images.slice -5}"><img src="#{images.slice -5}" width="128" height="96" /></a>
+            <a href="##{images.slice -6}"><img src="#{images.slice -6}" width="128" height="96" /></a>
+            <a href="##{images.slice -7}"><img src="#{images.slice -7}" width="128" height="96" /></a>
+            <a href="##{images.slice -8}"><img src="#{images.slice -8}" width="128" height="96" /></a>
           </div>
         </div>
       </div>
@@ -78,30 +85,29 @@ html = <<-end
       var load = function ( i ) {
         if ( i < 0 ) i = max;
         if ( i > max ) i = 0;
-        if ( images[i] && ( 'jpg' === images[i].substr( -3, 3 ))) {
-          location.hash = images[i];
-          image.title = images[i];
-          image.src = images[i];
-          if ( /(\\d+)x(\\d+)-(\\d+)x(\\d+)/.exec( images[i] )) {
-            if ( RegExp.$1 || RegExp.$2 || RegExp.$3 || RegExp.$4 ) {
-              var width = RegExp.$1 / 1;
-              var height = RegExp.$2 / 1;
-              var x = RegExp.$3 / 1;
-              var y = RegExp.$4 / 1;
-              var left = Math.round( x - ( width / 2 ));
-              var top = Math.round( y - ( height / 2 ));
-              highlight.style.visibility = 'visible';
-              highlight.style.border = '1px #ff0 dashed';
-              highlight.style.left = left + 'px';
-              highlight.style.top = top + 'px';
-              highlight.style.width = width + 'px';
-              highlight.style.height = height + 'px';
-            }
-            else {
-              highlight.style.visibility = 'hidden';
-            }
+        if ( ! images[i] ) return i;
+        if ( 'jpg' !== images[i].substr( -3, 3 )) return i;
+        location.hash = images[i];
+        image.title = images[i];
+        image.src = images[i];
+        if ( /(\\d+)x(\\d+)-(\\d+)x(\\d+)/.exec( images[i] )) {
+          if ( RegExp.$1 || RegExp.$2 || RegExp.$3 || RegExp.$4 ) {
+            var width = RegExp.$1 / 1;
+            var height = RegExp.$2 / 1;
+            var x = RegExp.$3 / 1;
+            var y = RegExp.$4 / 1;
+            var left = Math.round( x - ( width / 2 ));
+            var top = Math.round( y - ( height / 2 ));
+            highlight.style.visibility = 'visible';
+            highlight.style.border = '1px #ff0 dashed';
+            highlight.style.left = left + 'px';
+            highlight.style.top = top + 'px';
+            highlight.style.width = width + 'px';
+            highlight.style.height = height + 'px';
           }
-          return i;
+          else {
+            highlight.style.visibility = 'hidden';
+          }
         }
         return i;
       };
@@ -125,6 +131,8 @@ html = <<-end
       attachEvent( $( '#next' ), 'click', function ( e ) {
         index = load( ++ index );
       } );
+
+      // how does this work then?
       var touchStart = null;
       attachEvent( d, 'touchstart', function ( event ) {
         var err;
@@ -141,6 +149,7 @@ html = <<-end
       attachEvent( d, 'touchend', function ( event ) {
         alert( 'end ' + start + JSON.stringify( event ));
       } );
+
       if ( location.hash ) {
         index = load( images.indexOf( location.hash.substr( 1 )));
       }
