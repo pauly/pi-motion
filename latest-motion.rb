354ccc22009289b9d53a80027aa3ac64c5f7849e
pauly <pauly@clarkeology.com>
2014-10-10 22:14:09 +0100

    analytics

--- latest-motion.rb
+++ latest-motion.rb
@@ -159,6 +159,7 @@ html = <<-end
         index = load( images.indexOf( location.hash.substr( 1 )));
       }
     </script>
+    <script>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-266418-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script><script type="text/javascript" src="http://www.google.co.uk/coop/cse/brand?form=cse-search-box&amp;lang=en"></script>
   </body>
 </html>
 end
