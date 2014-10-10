311c7c2b4840751ddab706e609eb6f0fd8c82b20
pauly <pauly@clarkeology.com>
2014-10-10 12:46:34 +0100

    cron, ignore log, shorter image names

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-export EDITOR=vi
+echo "\nexport EDITOR=vi" >> ~/.profile
 sudo passwd pi
 sudo dpkg-reconfigure tzdata
 sudo apt-get update
@@ -6,9 +6,10 @@ sudo apt-get upgrade
 sudo apt-get install -y git-core apache2 php5 libapache2-mod-php5 libjpeg62 libjpeg62-dev libavformat53 libavformat-dev libavcodec53 libavcodec-dev libavutil51 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev
 # git config --global user.email "you@example.com"
 # git config --global user.name "Your Name"
-git clone https://github.com/pauly/pi-motion.git
-sudo cp motion.conf /etc/motion.conf
-sudo cp motion /usr/bin
+cd && git clone https://github.com/pauly/pi-motion.git
+sudo crontab cron.tab
+cd /etc && sudo ln -s ~/pi-motion/motion.conf .
+sudo /usr/sbin && sudo ln -s ~/pi-motion/motion .
 sudo cat /etc/rc.local | sed 's/exit/motion\n\nexit/' > /etc/rc.local
 # echo '\ndisable_camera_led=1' >> /boot/config.txt
 mkdir /var/www/motion
