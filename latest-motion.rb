c4ea8f3be263f4571d44d9def96956e70aab2d47
Pauly <pauly@clarkeology.com>
2016-02-29 15:40:38 +0000

    fixes

--- latest-motion.rb
+++ latest-motion.rb
@@ -5,19 +5,9 @@ require 'json'
 folder = ARGV.length ? ARGV[0] : Dir.pwd
 today = Time.now.strftime '%Y-%m-%d'
 minsize = 18000
-images = [ ]
 files = Dir.entries( folder ).sort_by { |a| File.stat( folder + a ).mtime }
-files.each do | f |
-  if f !~ /^\./
-    if 'jpg' === f[ f.length - 3, f.length - 1 ]
-      stat = File.stat( folder + '/' + f )
-      if stat.size > minsize
-        images << f
-      else
-        $stderr.puts 'delete ' + f + '???'
-      end
-    end
-  end
+images = files.reject do | f |
+  f[ f.length - 3, f.length - 1 ] != 'jpg'
 end
 date = Time.new.to_s
 title = 'Motion ' + date
@@ -32,9 +22,8 @@ html = <<-end
       h1 { font-size: 1em; }
       p, a.video { font-size: 0.8em; font-weight: normal; }
       #image-container { position: relative; }
-      #highlight { position: absolute; border: 1px #ff0 dashed; z-index: 3; }
-      #highlight a { color: #ff0; text-decoration: none; }
-      #hightlight.hidden { display: none; }
+      #highlight { position: absolute; display: block; border: 1px #ff0 dashed; z-index: 3; color: #ff0; text-decoration: none; }
+      #highlight.hidden { display: none; }
       #prev, #next { position: absolute; left: 0px; top: 0px; width: 512px; height: 576px; float: left; text-indent: 999em; }
       #next { margin-left: 512px; text-align: right; }
       #ads { width: 120px; float: right; margin: 10px; }
@@ -61,11 +50,11 @@ html = <<-end
             <script data="inline"> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
           </div>
           <h1><a href="">#{title}</a></h1>
-          <p>Way slow, loaded from a <a href="http://www.clarkeology.com/wiki/raspberry+pi">raspberry pi</a> in my house. Hmm, this is why my broadband appears so patchy...</p>
+          <p>Way slow, loaded from a <a href="http://www.clarkeology.com/wiki/raspberry+pi">raspberry pi</a> in my house. Hmm, this is why I had to upgrade to fibre...</p>
           <p><a href="https://github.com/pauly/pi-motion">See the repo</a> for how to set up your pi and <a href="http://www.amazon.co.uk/gp/product/B00E1GGE40/ref=as_li_tl?ie=UTF8&camp=1634&creative=19450&creativeASIN=B00E1GGE40&linkCode=as2&tag=clarkeology-21&linkId=ECALLBWP73WQVL7M">camera module</a> like this...</p>
           <div id="image-container">
             <img id="image" src="#{images.slice -1}" title="#{images.slice -1}"  width="1024" height="576" />
-            <div id="highlight"></div>
+            <a id="highlight"></a>
             <a id="prev">&lt;</a>
             <a id="next">&gt;</a>
           </div>
@@ -85,96 +74,86 @@ html = <<-end
     <p>Blank image? Looking for a demo? See <a href="http://pauly.github.io/pi-motion/">http://pauly.github.io/pi-motion/</a>...</p>
     <p>By <a href="http://www.clarkeology.com/blog/">Paul Clarke</a>, a work in progress.</p>
     <script type="text/javascript">
-      if ( ! window.console ) window.console = { log: function ( ) { alert( arguments ); }};
       var d = document;
-      var $ = function ( id ) {
-        return d.getElementById(( '' + id ).substr( 1 ));
+      var attachEvent = function(e, event, callback) {
+        if (e.addEventListener) return e.addEventListener(event, callback, false);
+        if (e.attachEvent) return e.attachEvent('on' + event, callback);
       };
-      var attachEvent = function ( e, event, callback ) {
-        if ( e.addEventListener ) return e.addEventListener( event, callback, false );
-        if ( e.attachEvent ) return e.attachEvent( 'on' + event, callback );
-      };
-      var image = $( '#image' );
-      var highlight = $( '#highlight' );
-      var images = #{images.to_json};
-      var max = images.length - 1;
+      var image = d.getElementById('image');
+      var highlight = d.getElementById('highlight');
+      var files = #{files.to_json};
+      var max = files.length - 1;
       var index = max;
-      var load = function ( i ) {
-        if ( i < 0 ) i = max;
-        if ( i > max ) i = 0;
-        if ( ! images[i] ) return i;
-        if ( images[i].substr( -3, 3 ) !== 'jpg' ) return i;
-        location.hash = images[i];
-        image.title = images[i];
-        image.src = images[i];
-        if ( /(\\d+)x(\\d+)-(\\d+)x(\\d+)/.exec( images[i] )) {
-          if ( RegExp.$1 || RegExp.$2 || RegExp.$3 || RegExp.$4 ) {
-            var width = RegExp.$1 / 1;
-            var height = RegExp.$2 / 1;
-            var x = RegExp.$3 / 1;
-            var y = RegExp.$4 / 1;
-            var left = Math.round( x - ( width / 2 ));
-            var top = Math.round( y - ( height / 2 ));
-            highlight.style.left = left + 'px';
-            highlight.style.top = top + 'px';
-            highlight.style.width = width + 'px';
-            highlight.style.height = height + 'px';
-          }
-          else {
-            highlight.className = 'hidden';
-          }
+      var load = function(i) {
+        if (i < 0) i = max;
+        if (i > max) i = 0;
+        if (! files[i]) return i;
+        location.hash = files[i];
+        image.title = files[i];
+        var extension = files[i].substr(-3, 3);
+        if (extension === 'jpg') {
+          image.src = files[i];
         }
-        // -rw-r--r-- 1 pi pi  29358 Jun  4 20:45 04204518-30x155-14x250-1667-e02-q00.jpg
-        // -rw-r--r-- 1 pi pi  29185 Jun  4 20:46 04204518-30x155-14x250-1667-e02-snapshot.jpg
-        // -rw-r--r-- 1 pi pi  78724 Jun  4 20:46 04204518-30x155-14x250-1667-02.avi
-        if ( /(.+-)e(\\d+)-snapshot/.exec( images[i] )) {
-          var video = RegExp.$1 + RegExp.$2 + '.avi';
-          highlight.innerHTML = '<a class="video" href="' + video + '">video</a>';
+        highlight.innerHTML = '';
+        highlight.className = '';
+        if (extension === 'avi') {
+          highlight.href = files[i];
+          highlight.innerHTML = 'video';
+          highlight.className = 'video';
         }
-
+        if (/(\\d+)x(\\d+)-(\\d+)x(\\d+)/.exec(files[i])) {
+          if (!(RegExp.$1 && RegExp.$2 && RegExp.$3 && RegExp.$4)) return i;
+          var width = RegExp.$1 / 1;
+          var height = RegExp.$2 / 1;
+          var x = RegExp.$3 / 1;
+          var y = RegExp.$4 / 1;
+          var left = Math.round(x - (width / 2));
+          var top = Math.round(y - (height / 2));
+          highlight.style.left = left + 'px';
+          highlight.style.top = top + 'px';
+          highlight.style.width = width + 'px';
+          highlight.style.height = height + 'px';
+	  return i;
+        }
+        highlight.className = 'hidden';
         return i;
       };
-      attachEvent( d, 'keydown', function ( e ) {
-        switch ( e.which || e.keyDown ) {
+      attachEvent(d, 'keydown', function(e) {
+        switch (e.which || e.keyDown) {
           case 37:
           case 38:
-            index = load( -- index );
-            e.preventDefault( );
+            e.preventDefault();
+            index = load(--index);
             break;
           case 39:
           case 40:
-            index = load( ++ index );
-            e.preventDefault( );
+            e.preventDefault();
+            index = load(++index);
             break;
         }
-      } );
-      attachEvent( $( '#prev' ), 'click', function ( e ) {
-        index = load( -- index );
-      } );
-      attachEvent( $( '#next' ), 'click', function ( e ) {
-        index = load( ++ index );
-      } );
+      });
+      attachEvent(d.getElementById('prev'), 'click', function(e) {
+        index = load(--index);
+      });
+      attachEvent(d.getElementById('next'), 'click', function(e) {
+        index = load(++index);
+      });
 
-      // how does this work then?
-      var touchStart = null;
-      attachEvent( d, 'touchstart', function ( event ) {
-        var err;
-        try {
-          touchStart = event.originalEvent.touches[0].pageX
-        }
-        catch ( err ) {
-        }
-        alert( 'start ' + touchStart + ' ' + JSON.stringify( event ) + ' ' + err );
+      var startEvent;
+      attachEvent(d, 'touchstart', function(event) {
+        startEvent = event;
+      });
+      attachEvent(d, 'touchmove', function(event) {
+	event.preventDefault();
+        alert('move ' + JSON.stringify(event) + ' ' + JSON.stringify(originalEvent));
       } );
-      attachEvent( d, 'touchmove', function ( event ) {
-        alert( 'move ' + start + JSON.stringify( event ));
-      } );
-      attachEvent( d, 'touchend', function ( event ) {
-        alert( 'end ' + start + JSON.stringify( event ));
-      } );
+      attachEvent(d, 'touchend', function(event) {
+	event.preventDefault();
+        alert('end ' + JSON.stringify(event) + ' ' + JSON.stringify(originalEvent));
+      });
 
-      if ( location.hash ) {
-        index = load( images.indexOf( location.hash.substr( 1 )));
+      if (location.hash) {
+        index = load(files.indexOf(location.hash.substr(1)));
       }
     </script>
     <script>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-266418-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script><script type="text/javascript" src="http://www.google.co.uk/coop/cse/brand?form=cse-search-box&amp;lang=en"></script>
